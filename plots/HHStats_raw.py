3857a1ae29e448719e2997fd41c7b9ca2ada63f6
IzaakWN <iwn_@hotmail.com>
2015-10-30 00:57:27 +0100

    Real window cuts.

--- plots/HHStats_raw.py
+++ plots/HHStats_raw.py
@@ -31,23 +31,23 @@ def punzi(stage):
     print "\n   " + stage + \
           " Punzi significance" + \
           "\n  ----------------------------------------------------------" + \
-          "\n   P     \tS \tB\t Mass reco" + \
+          "\n   P     \tS \tB\t\t Mass reco" + \
           "\n  ----------------------------------------------------------"
     
-    M = [ ("jj",0,120),
-          ("jj_cut",0,120),
-          ("jj_leading",0,120),
-          ("jj_leading_cut",0,120),
-          ("jj_b2b",0,120),
-          ("jj_b2b_cut",0,120),
-          ("jjl",0,120),
+    M = [ ("jj",0,0),
+          ("jj_cut",0,0),
+          ("jj_leading",0,0),
+          ("jj_leading_cut",0,0),
+          ("jj_b2b",0,0),
+          ("jj_b2b_cut",0,0),
+          ("jjl",0,160),
           ("bb",0,120),
-          ("bb_cut",0,120),
+          ("bb_cut",70,140),
           ("bb_leading",0,120),
           ("bb_leading_cut",0,120),
-          ("bb_closest",0,120),
-          ("bb_closest_cut",0,120),
-          ("b1l",0,120) ]
+          ("bb_closest",80,145),
+          ("bb_closest_cut",80,145),
+          ("b1l",0,150) ]
     
     
     
@@ -61,14 +61,15 @@ def punzi(stage):
         
         P = round( N_S * (S/S_tot) / (1+sqrt( N_B * (B/B_tot) )), 5 )
         print "   %s\t%.2f\t%.2f\t" % ( P, N_S*(S/S_tot), N_B*(B/B_tot) ) + "M_" + obj
-        
-        S = hist_S.Integral(hist_S.GetXaxis().FindBin(x1),hist_S.GetXaxis().FindBin(x2))
-        B = hist_S.Integral(hist_B.GetXaxis().FindBin(x1),hist_S.GetXaxis().FindBin(x2))
-        
-        P = round( N_S * (S/S_tot) / (1+sqrt( N_B * (B/B_tot) )), 5 )
-        print "   %s\t%.2f\t%.2f\t" % ( P, N_S*(S/S_tot), N_B*(B/B_tot) ) + "M_" + obj + " window (%i,%i)" % (x1,x2)
+            
+        if x2:
+            S = hist_S.Integral(hist_S.GetXaxis().FindBin(x1),hist_S.GetXaxis().FindBin(x2))
+            B = hist_S.Integral(hist_B.GetXaxis().FindBin(x1),hist_S.GetXaxis().FindBin(x2))
+            
+            P = round( N_S * (S/S_tot) / (1+sqrt( N_B * (B/B_tot) )), 5 )
+            print "   %s\t%.2f\t%.2f\t" % ( P, N_S*(S/S_tot), N_B*(B/B_tot) ) + "M_" + obj + " window (%i,%i)" % (x1,x2)
 
-    print "\n\n"
+    print "\n"
 
 
     ########
