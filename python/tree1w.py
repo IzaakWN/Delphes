565e62fc8abfadc77684c826abc87803192df636
IzaakWN <iwn_@hotmail.com>
2015-11-03 17:46:49 +0100

    Correct tree writing in pyroot

--- python/tree1w.py
+++ python/tree1w.py
@@ -1,26 +1,29 @@
 from ROOT import TFile, TTree, TBranch, TRandom
 
+# http://wlav.web.cern.ch/wlav/pyroot/tpytree.html
+# ftp://root.cern.ch/root/doc/ROOTUsersGuideHTML/ch12s14.html
+# https://root.cern.ch/root/html/tutorials/tree/tree1.C.html
 
 def tree1w():
 
     # create a tree file tree1.root - create the file, the Tree and a few branches
     f = TFile("tree1.root","recreate")
     t1 = TTree("t1","a simple Tree with simple variables")
-    px = 0.0
-    py = 0.0
-    pz = 0.0
-    ev = 0
-    t1.Branch("px",&px,"px/F")
-    t1.Branch("py",&py,"py/F")
-    t1.Branch("pz",&pz,"pz/F")
-    t1.Branch("ev",&ev,"ev/I")
+    px = array('f', [0])
+    py = array('f', [0])
+    pz = array('f', [0])
+    ev = array('i', [0])
+    t1.Branch("px",px,"px/F")
+    t1.Branch("py",py,"py/F")
+    t1.Branch("pz",pz,"pz/F")
+    t1.Branch("ev",ev,"ev/I")
 
     # fill the tree
     for i in range(0,1000):
-        gRandom.Rannor(px,py)
-        pz = px*px + py*py
+        gRandom.Rannor(px[0],py[0])
+        pz[0] = px[0]*px[0] + py[0]*py[0]
         random = gRandom.Rndm()
-        ev = i
+        ev[0] = i
         t1.Fill()
 
     # save the Tree heade the file will be automatically closed
