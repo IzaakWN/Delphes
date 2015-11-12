5ada4f475fe9dbc10dfdd25625b20fd344661a0a
IzaakWN <iwn_@hotmail.com>
2015-11-12 11:55:19 +0100

    Extra plots.

--- plots/HH_MVA.py
+++ plots/HH_MVA.py
@@ -32,9 +32,9 @@ parser.add_option("-p", "--onlyPlot", dest="onlyPlot", default=False, action="st
     
 f_in_HH = TFile("/shome/ineuteli/phase2/CMSSW_5_3_24/src/Delphes/controlPlots_HH_all.root")
 f_in_tt = TFile("/shome/ineuteli/phase2/CMSSW_5_3_24/src/Delphes/controlPlots_tt_all.root")
-
 treeS = f_in_HH.Get("stage_2/cleanup/cleanup")
 treeB = f_in_tt.Get("stage_2/cleanup/cleanup")
+methods = [ ]
 
 
 
@@ -42,9 +42,10 @@ class configuration(object):
 
     def __init__(self, name="nameless"):
         self.name = name
-        self.varNames = []
+        self.varNames = [ ]
         self.treeS = treeS
         self.treeB = treeB
+        self.hist_effs = [ ]
 
 
 
@@ -88,7 +89,7 @@ def train(config):
                                            "SeparationType=GiniIndex",
                                            "nCuts=20",
                                            "PruneMethod=NoPruning" ]) )
-                                           
+
     method = factory.BookMethod(TMVA.Types.kBDT, "BDTTuned",
                                 ":".join([ "!H",
                                            "!V",
@@ -98,7 +99,10 @@ def train(config):
                                            "BoostType=AdaBoost",
                                            "AdaBoostBeta=0.5",
                                            "SeparationType=GiniIndex",
-                                           "nCuts=50" ]) )
+                                           "nCuts=20" ]) )
+    
+    global methods
+    methods = [ ("BDT","BDT"), ("BDT","BDTTuned") ]
  
     factory.TrainAllMethods()
     factory.TestAllMethods()
@@ -107,7 +111,7 @@ def train(config):
 
 
 
-def examine(config):
+def plot(config):
     print "\n>>> examine training with configuration "+config.name
 
     reader = TMVA.Reader()
@@ -119,16 +123,14 @@ def examine(config):
         vars.append(array('f',[0]))
         reader.AddVariable(name,vars[-1])
 
-    hist_effs = [ ]
-    methods = [ ("BDT","BDT"), ("BDT","BDTTuned") ]
+    # HISTOGRAMS: TMVA output
     for Method, method in methods:
         reader.BookMVA(method,"weights/TMVAClassification_"+method+".weights.xml")
 
-        # fill histograms for signal and background from the test sample tree
         c = makeCanvas()
         histS = TH1F("histS", "", 44, -1.1, 1.1)
         histB = TH1F("histB", "", 44, -1.1, 1.1)
-        hist_effs.append(gDirectory.Get("Method_"+Method+"/"+method+"/MVA_"+method+"_rejBvsS"))
+        config.hist_effs.append(gDirectory.Get("Method_"+Method+"/"+method+"/MVA_"+method+"_rejBvsS"))
         TestTree.Draw(method+">>histS","classID == 0","goff") # causes problem when training not run
         TestTree.Draw(method+">>histB","classID == 1", "goff")
 #        entries = mychain.GetEntriesFast()
@@ -145,10 +147,11 @@ def examine(config):
         histB.SetLineColor(ROOT.kBlue)
         histB.SetLineWidth(2)
         histB.SetStats(0)
+        histB.GetXaxis().SetTitle(method+" output")
 
         histB.Draw() # draw first: mostly bigger
         histS.Draw("same")
-        legend = makeLegend(histS,histB,title="MVA seperation",entries=["signal","background"])
+        legend = makeLegend(histS,histB,title=method+" seperation",entries=["signal","background"])
         legend.Draw()
 
         CMS_lumi.CMS_lumi(c,14,33)
@@ -158,21 +161,60 @@ def examine(config):
         gDirectory.Delete("histB")
 
 
+
+# HISTOGRAMS: compare all methods and variable configurations
+def compare(configs):
+    
+    hists_effs = [ ]
+    for config in configs:
+        hist_effs.extend(config.hist_effs)
+    
+    if not hists_effs:
+        return
+    
     c = makeCanvas()
     hist_effs[0].Draw()
-    makeAxes(hist_effs[0],xlabel="signal efficiency",ylabel="background rejection")
     hist_effs[0].SetLineWidth(2)
     hist_effs[0].SetStats(0)
+    makeAxes(hist_effs[0],xlabel="signal efficiency",ylabel="background rejection")
     if len(hist_effs)>1:
         for hist in hist_effs[1:]:
             hist.Draw("same")
             hist.SetLineWidth(2)
+    labels = [ ]
+    for config in configs:
+        labels.extend([config.name", "+method[1] for method in methods])
     legend = makeLegend(*hist_effs,title="#splitline{background rejection}{vs. signal efficiency}",
-                                   entries=[method[1] for method in methods], position="RightBottom")
+                                   entries=labels, position="RightTop")
     legend.Draw()
     setLineColor(*hist_effs)
     CMS_lumi.CMS_lumi(c,14,33)
-    c.SaveAs("MVA/HH_MVA_BrejvsSeff_"+config.name+".png")
+    c.SaveAs("MVA/HH_MVA_BrejvsSeffs_"+config.name+".png")
+    c.Close()
+
+
+
+# 2D COLOR HISTOGRAM: Correlation matrices
+def correlation(config):
+
+    reader = TMVA.Reader()
+    f = TFile("HH_MVA_"+config.name+".root")
+    TestTree = gDirectory.Get("TestTree")
+
+    c = makeCanvas(square=True)
+    histS = f.Get("CorrelationMatrixS.png")
+    histS.Draw("colz")
+    makeAxes2D(histS)
+    CMS_lumi.CMS_lumi(c,14,33)
+    c.SaveAs("MVA/CorrelationMatrixS.png")
+    c.Close()
+
+    c = makeCanvas(square=True)
+    histB = f.Get("CorrelationMatrixB.png")
+    histB.Draw("colz")
+    makeAxes2D(histB)
+    CMS_lumi.CMS_lumi(c,14,33)
+    c.SaveAs("MVA/CorrelationMatrixB.png")
     c.Close()
 
 
@@ -202,7 +244,9 @@ def main():
     for config in configs:
         if not opts.onlyPlot:
             train(config)
-        examine(config)
+        plot(config)
+    compare(configs)
+    correlation(configs[0])
 
 
 
