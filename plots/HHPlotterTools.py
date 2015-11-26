6709325f7352f2f4b4ca37a8c4c00463d1dc5dd9
IzaakWN <iwn_@hotmail.com>
2015-11-26 22:13:57 +0100

    Stuff.

--- plots/HHPlotterTools.py
+++ plots/HHPlotterTools.py
@@ -367,8 +367,8 @@ def makeAxes(*hists, **kwargs):
     xlabel = kwargs.get('xlabel', "")
     ylabel = kwargs.get('ylabel', "")
     
-    hist0.GetXaxis().SetTitleSize(0.055,"x")
-    hist0.GetXaxis().SetTitleSize(0.05,"y")
+    hist0.GetXaxis().SetTitleSize(0.055)
+    hist0.GetYaxis().SetTitleSize(0.05)
     
     # make correct x-axis labels
     if xlabel + ylabel:
