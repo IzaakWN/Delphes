a1a55208df879b0c28b1eb181596eb19e2a6d328
IzaakWN <iwn_@hotmail.com>
2015-12-03 23:20:37 +0100

    Stuff.

--- python/HHEventSelection.py
+++ python/HHEventSelection.py
@@ -91,12 +91,14 @@ def eventCategory(event):
     M_bb = 0
     DeltaR_bb = 100
     p_bb = None
+    bjet_closest = [ ]
     for b1,b2 in combinations(event.bjets30,2):
         DeltaR = TLV.DeltaR(b1.TLV,b2.TLV)
         if DeltaR < DeltaR_bb:
             DeltaR_bb = DeltaR
             p_bb = b1.TLV+b2.TLV
             M_bb = p_bb.M()
+            bjet_closest = [b1,b2]
     categoryData.append( 60 < M_bb < 160 and \
                          DeltaR_bb < 3 )
 
@@ -108,7 +110,8 @@ def eventCategory(event):
         DeltaR_bl = max(TLV.DeltaR(bj.TLV,leps[0].TLV) for bj in event.bjets30)
         if p_bb and len(event.cleanedJets20)>3:
             # closest jet-lepton pair
-            jet = min([j for j in event.cleanedJets20 if not j.BTag], key=lambda j: TLV.DeltaR(j.TLV,leps[0].TLV))
+            jet = min([j for j in event.cleanedJets20 if j not in bjet_closest],
+                                      key=lambda j: TLV.DeltaR(j.TLV,leps[0].TLV))
             DeltaPhi_j1lbb = fold(abs( (jet.TLV+leps[0].TLV).Phi() - p_bb.Phi() ))
 #            DeltaR_j1lbb = TLV.DeltaR(jet.TLV+lepton.TLV, p_bb.Phi())
     categoryData.append( categoryData[-1] and \
