f28e865e37df177bd4ba8eae56df338cf77a88be
Izaak Neutelings <iwn_@hotmail.com>
2015-12-10 18:10:49 +0100

    Changes from UZH

--- plots/HHttPlotter.py
+++ plots/HHttPlotter.py
@@ -153,6 +153,9 @@ def plotBasic(stage):
         elif name[name.index("/")+1:] in [ "DeltaR_bb", "DeltaR_j2l", "DeltaPhi_bb" ]: #"DeltaR_bb1",
             legend = makeLegend(hist_S,hist_tt,title=" ",tt=True,position="RightRightTopTop",transparent=True)
             legend.Draw()
+        elif name[name.index("/")+1:] in [ "DeltaR_jj" ]: #"DeltaR_bb1",
+            legend = makeLegend(hist_S,hist_tt,title=" ",tt=True,position="CenterBottom",transparent=True)
+            legend.Draw()
 #        elif "_jj" in name:
 #            legend = makeLegend(hist_S,hist_tt,tt=True,position="RightTop")
 #            legend.Draw()
