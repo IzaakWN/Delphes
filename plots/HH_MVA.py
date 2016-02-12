123bacfb38343b2199f4083698e64d2fc73f6e4d
Izaak Neutelings <IWN@iwnsmbp.local>
2016-02-12 02:47:26 +0100

    Stuff.

--- plots/HH_MVA.py
+++ plots/HH_MVA.py
@@ -527,10 +527,10 @@ def significance(config,histS,histB,test=False):
     cut = histS.GetXaxis().GetBinCenter(imax)
     Pbins = significanceBins(config,histS,histB)
 
-    prompt = ">>> "+config.name+" - "+method + " (test sample):" + \
-             ("\n>>>\t\t%.4f significance (%.4f with bins) with yields" % (Pmax,Pbins)) + \
-             ("\n>>>\t\tS = %.1f, B = %.1f and a cut at %.3f" % (Smax,Bmax,cut)) + \
-             ("\n>>>\t\t(Si=%.f (%.2f%%) and Bi=%.f (%.4f%%))" % (Simax,effS,Bimax,effB))
+    prompt = (">>> "+config.name+" - "+method + " (test sample):" + \
+              "\n>>>\t\t%.4f significance (%.4f with bins) with yields" % (Pmax,Pbins) + \
+              "\n>>>\t\tS = %.1f, B = %.1f and a cut at %.3f." % (Smax,Bmax,cut) + \
+              "\n>>>\t\t(Si=%.f (%.2f%%) and Bi=%.f (%.4f%%))" % (Simax,effS,Bimax,effB))
 
     if test:
         sample = " test"
