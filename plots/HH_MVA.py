df5afbac9fa4efeec0200903d89f1fb2dd9a6382
Izaak Neutelings <IWN@iwnsmbp.local>
2016-02-12 03:25:40 +0100

    Stuff.

--- plots/HH_MVA.py
+++ plots/HH_MVA.py
@@ -531,9 +531,10 @@ def significance(config,histS,histB,test=False):
     else:
         sample = "total"
 
-    prompt =  ">>> "+config.name+" - "+method+" (test sample):" + \
-              ("\n>>>\t\t%.4f significance (%.4f with bins) with yields" % (Pmax,Pbins)) + \
-              ("\n>>>\t\tS = %.1f, B = %.1f and a cut at %.3f." % (Smax,Bmax,cut)) + \
+    prompt = []
+    prompt.append(">>> "+config.name+" - "+method+" (test sample):" + \
+              "\n>>>\t\t%.4f significance (%.4f with bins) with yields" % (Pmax,Pbins) + \
+              "\n>>>\t\tS = %.1f, B = %.1f and a cut at %.3f." % (Smax,Bmax,cut) + \
               "\n>>>\t\t(Si=%.f (%.2f%%) and Bi=%.f (%.4f%%))" % (Simax,effS,Bimax,effB)
 
     #      METHOD | SAMPLE |   P  |   S   |   B   |  Si  |  Bi  |    eS   |    eB  |  cut | Pbins |
